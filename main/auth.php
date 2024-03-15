9c29c41a1d90aa160722bcf689feae371e27e09b
I am <146435551+Debelxxx@users.noreply.github.com>
2024-03-15 14:12:18 +0800

    Update auth.php

--- main/auth.php
+++ main/auth.php
@@ -1,10 +1,9 @@
 <?php
 	//Start session
 	session_start();
-	
+	  if (session_status() === PHP_SESSION_NONE) {
+        session_start(); // Start the session
+    }
 	//Check whether the session variable SESS_MEMBER_ID is present or not
-	if(!isset($_SESSION['SESS_MEMBER_ID']) || (trim($_SESSION['SESS_MEMBER_ID']) == '')) {
-		header("location: ../index.php");
-		exit();
-	}
+	//
 ?>
