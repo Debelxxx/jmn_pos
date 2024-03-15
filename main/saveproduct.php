ddcc7c3188c974de84d5aa63a173fb6713293873
I am <146435551+Debelxxx@users.noreply.github.com>
2024-03-15 14:13:07 +0800

    Update saveproduct.php

--- main/saveproduct.php
+++ main/saveproduct.php
@@ -2,21 +2,20 @@
 session_start();
 include('../connect.php');
 $a = $_POST['code'];
-$b = $_POST['name'];
-$c = $_POST['exdate'];
-$d = $_POST['price'];
-$e = $_POST['supplier'];
-$f = $_POST['qty'];
-$g = $_POST['o_price'];
-$h = $_POST['profit'];
-$i = $_POST['gen'];
-$j = $_POST['date_arrival'];
-$k = $_POST['qty_sold'];
-// query
-$sql = "INSERT INTO products (product_code,product_name,expiry_date,price,supplier,qty,o_price,profit,gen_name,date_arrival,qty_sold) VALUES (:a,:b,:c,:d,:e,:f,:g,:h,:i,:j,:k)";
+$b = $_POST['desc'];
+$c = $_POST['price'];
+$d = $_POST['supplier'];
+$e = $_POST['qty'];
+$f = $_POST['o_price'];
+$g = $_POST['profit'];
+$h = $_POST['name'];
+$i = $_POST['date_arrival'];
+$j = $_POST['qty_sold'];
+
+// query with backticks around reserved keyword 'desc'
+$sql = "INSERT INTO products (product_code, `desc`, price, supplier, qty, o_price, profit, name, date_arrival, qty_sold) 
+        VALUES (:a, :b, :c, :d, :e, :f, :g, :h, :i, :j)";
 $q = $db->prepare($sql);
-$q->execute(array(':a'=>$a,':b'=>$b,':c'=>$c,':d'=>$d,':e'=>$e,':f'=>$f,':g'=>$g,':h'=>$h,':i'=>$i,':j'=>$j,':k'=>$k));
+$q->execute(array(':a'=>$a,':b'=>$b,':c'=>$c,':d'=>$d,':e'=>$e,':f'=>$f,':g'=>$g,':h'=>$h,':i'=>$i,':j'=>$j));
 header("location: products.php");
-
-
 ?>
