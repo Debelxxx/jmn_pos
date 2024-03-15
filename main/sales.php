08a4510cbb13dfc3bce776901c3cd1cdbcea0808
I am <146435551+Debelxxx@users.noreply.github.com>
2024-03-15 14:13:02 +0800

    Update sales.php

--- main/sales.php
+++ main/sales.php
@@ -129,6 +129,7 @@ if($position=='admin') {
 			<li><a href="products.php"><i class="icon-list-alt icon-2x"></i> Products</a>                                     </li>
 			<li><a href="customer.php"><i class="icon-group icon-2x"></i> Customers</a>                                    </li>
 			<li><a href="supplier.php"><i class="icon-group icon-2x"></i> Suppliers</a>                                    </li>
+			<li><a href="inventory.php"><i class="icon-list-alt icon-2x"></i>Inventory</a> </li>
 			<li><a href="salesreport.php?d1=0&d2=0"><i class="icon-bar-chart icon-2x"></i> Sales Report</a>                </li>
 			<br><br><br><br><br><br>
 			<li>
