6988d74faa2f36270c49e7ca070ae73880211003
I am <146435551+Debelxxx@users.noreply.github.com>
2024-03-15 14:12:26 +0800

    Update customer.php

--- main/customer.php
+++ main/customer.php
@@ -109,6 +109,7 @@ $finalcode='RS-'.createRandomPassword();
 			<li><a href="products.php"><i class="icon-list-alt icon-2x"></i> Products</a>                                     </li>
 			<li class="active"><a href="customer.php"><i class="icon-group icon-2x"></i> Customers </a>                                    </li>
 			<li><a href="supplier.php"><i class="icon-group icon-2x"></i> Suppliers</a>                                    </li>
+			<li><a href="inventory.php"><i class="icon-list-alt icon-2x"></i>Inventory</a> </li>
 			<li><a href="salesreport.php?d1=0&d2=0"><i class="icon-bar-chart icon-2x"></i> Sales Report</a>                </li>
 					<br><br><br><br><br><br>		
 			<li>
